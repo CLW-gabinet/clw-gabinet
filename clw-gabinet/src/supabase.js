f2e3f5cb60a80f133295226fafb305b6b3467f1e
CLW <clw.gabinet@gmail.com>
2026-05-29 16:33:42 +0200

    fix: env variables

--- clw-gabinet/src/supabase.js
+++ clw-gabinet/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://clw-gabinet.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxbGRmbnVrZHBsaGhxaXFpbGpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5NTAxNTksImV4cCI6MjA5MzUyNjE1OX0.50uPv9cYLWJ0LVpM6Q_QYXDnVCeXBk3bIdYxkzwknVI'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
